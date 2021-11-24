04caf0d6a7f1268776e695e932bc3a7eb0a3bdfe
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-11-24 21:41:29 +0900

    [add] querybuilder

--- src/shared/store/store.repository.ts
+++ src/shared/store/store.repository.ts
@@ -1,5 +1,23 @@
+import { PostStoreDto } from 'src/store/request/post.store';
 import { EntityRepository, Repository } from 'typeorm';
+import { Category } from '../category/category.entity';
+import { Owner } from '../owner/owner.entity';
 import { Store } from './store.entity';
 
 @EntityRepository(Store)
-export class StoreRepository extends Repository<Store> {}
+export class StoreRepository extends Repository<Store> {
+  async savePost(postStoreDto: PostStoreDto, owner: Owner, category: Category) {
+    this.createQueryBuilder()
+      .insert()
+      .into(Store)
+      .values({
+        name: postStoreDto.name,
+        tel: postStoreDto.tel,
+        photoUrl: postStoreDto.photoUrl,
+        address: postStoreDto.address,
+        ownerId: owner,
+        categoryId: category,
+      })
+      .execute();
+  }
+}
