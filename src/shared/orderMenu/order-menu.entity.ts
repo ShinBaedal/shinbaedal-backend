8caa79df10f3cefd2bed6860e219be50caa751a2
pjp0704 <junhyeong25@gmail.com>
2021-11-24 17:02:57 +0900

    [ADD] OrderMenu entity

---
+++ src/shared/orderMenu/order-menu.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Order } from '../order/order.entity';
+import { Menu } from '../menu/menu.entity';
+
+@Entity()
+export class OrderMenu {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @ManyToOne((type) => Order, (order) => order.id)
+  orderId: Order;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @ManyToOne((type) => Menu, (menu) => menu.id)
+  menuId: Menu;
+}
