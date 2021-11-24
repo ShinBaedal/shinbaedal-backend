e57e6a0ffebbcd5198a9db0781615c64c562fef8
pjp0704 <junhyeong25@gmail.com>
2021-11-24 17:25:17 +0900

    [ADD] ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,37 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOption } from './ormconfig';
+import { Category } from './shared/category/category.entity';
+import { Client } from './shared/client/client.entity';
+import { Order } from './shared/order/order.entity';
+import { Owner } from './shared/owner/owner.entity';
+import { ReviewPhoto } from './shared/reviewPhoto/review-photo.entity';
+import { Reply } from './shared/reply/reply.entity';
+import { Store } from './shared/store/store.entity';
+import { OrderMenu } from './shared/orderMenu/order-menu.entity';
+import { Menu } from './shared/menu/menu.entity';
+import { Review } from './shared/review/review.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       ...connectionOption,
-      entities: [],
+      entities: [
+        Category,
+        Client,
+        Menu,
+        Order,
+        OrderMenu,
+        Owner,
+        Reply,
+        Review,
+        ReviewPhoto,
+        Store,
+      ],
     }),
   ],
   controllers: [],
