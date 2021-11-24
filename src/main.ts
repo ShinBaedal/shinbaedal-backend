e75b9720ba1e5c72fa7d35f69ab04d5c3d6b0622
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-11-24 20:54:55 +0900

    [add] exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
+import { HttpExceptionFilter } from './filter/HttpExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
   });
-
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
