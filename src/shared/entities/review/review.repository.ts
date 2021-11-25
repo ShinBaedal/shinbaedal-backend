453b5df66d50b3b9872ff86205c7a5950a995b42
pjp0704 <junhyeong25@gmail.com>
2021-11-25 13:26:42 +0900

    [ADD] isReviewExist, insertOneReview 메서드 추가

--- src/shared/entities/review/review.repository.ts
+++ src/shared/entities/review/review.repository.ts
@@ -1,6 +1,6 @@
-import { Type } from 'src/shared/enums/review.type';
 import { EntityRepository, Repository } from 'typeorm';
 import { Review } from './review.entity';
+import { CreateReviewPayload } from '../../../review/interfaces/create-review-payload.interface';
 
 @EntityRepository(Review)
 export class ReviewRepository extends Repository<Review> {
@@ -11,7 +11,31 @@ export class ReviewRepository extends Repository<Review> {
         t: 'MALIGNITY',
         store_id: store_id,
       })
-
       .getRawOne();
+  }
+
+  async isReviewExist(email: string, orderId: string): Promise<number> {
+    return await this.createQueryBuilder('review')
+      .select()
+      .leftJoinAndSelect('review.orderId', 'orderId')
+      .leftJoinAndSelect('review.clientId', 'clientId')
+      .where('clientId.email = :email', { email })
+      .andWhere('orderId.id = :orderId', { orderId })
+      .getCount();
+  }
+
+  async insertOneReview(payload: CreateReviewPayload) {
+    await this.createQueryBuilder()
+      .insert()
+      .into(Review)
+      .values({
+        content: payload.content,
+        rate: payload.rate,
+        type: payload.type,
+        clientId: () => payload.clientId.toString(),
+        storeId: () => payload.storeId.toString(),
+        orderId: () => payload.orderId.toString(),
+      })
+      .execute();
   }
 }
