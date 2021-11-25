8840daf4c09991ae80cdc1e6bad328b2d4376659
pjp0704 <junhyeong25@gmail.com>
2021-11-25 15:16:40 +0900

    [FEAT] 리뷰 type별 필터링

--- src/review/dto/request/review-list.dto.ts
+++ src/review/dto/request/review-list.dto.ts
@@ -1,6 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsEnum, IsString } from 'class-validator';
 
-export class ReviewListRequestDto {
+export class ReviewListRequestParamDto {
   @IsString()
   storeId: string;
+}
+
+export class ReviewListRequestQueryDto {
+  @IsEnum(['POSITIVE', 'NEUTRAL', 'NEGATIVE'])
+  type: string;
 }
