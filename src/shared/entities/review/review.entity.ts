feb73b0115b18ba73901d794217aec38260cc8a3
pjp0704 <junhyeong25@gmail.com>
2021-11-25 13:27:08 +0900

    [STYLE] 불필요한 import 삭제

--- src/shared/entities/review/review.entity.ts
+++ src/shared/entities/review/review.entity.ts
@@ -10,7 +10,6 @@ import {
 import { Client } from '../client/client.entity';
 import { Store } from '../store/store.entity';
 import { Order } from '../order/order.entity';
-import { Type } from 'src/shared/enums/review.type';
 
 @Entity()
 export class Review {
