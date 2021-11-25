e30b85f4296f22db7310a13292a39bf7949177ae
pjp0704 <junhyeong25@gmail.com>
2021-11-25 15:47:36 +0900

    [ADD] insertOneOrder

--- src/shared/entities/order/order.repository.ts
+++ src/shared/entities/order/order.repository.ts
@@ -1,5 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Order } from './order.entity';
+import { CreateOrderPayload } from '../../../order/interfaces/create-order-payload.interface';
 
 @EntityRepository(Order)
 export class OrderRepository extends Repository<Order> {
@@ -11,5 +12,18 @@ export class OrderRepository extends Repository<Order> {
       .where('order.id = :orderId', { orderId })
       .andWhere('clientId.email = :email', { email })
       .getOne();
+  }
+
+  async insertOneOrder(payload: CreateOrderPayload): Promise<void> {
+    await this.createQueryBuilder()
+      .insert()
+      .into(Order)
+      .values({
+        isDone: false,
+        clientId: () => payload.clientId.toString(),
+        storeId: () => payload.storeId.toString(),
+        orderMenu: payload.orderMenu,
+      })
+      .execute();
   }
 }
